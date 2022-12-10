c5e8b4419e9e1584e4ede322503d5c44d3de1a7e
Sofya18 <119758211+Sofya18@users.noreply.github.com>
2022-12-10 23:39:59 +0300

    Task 19

---
+++ Task19/Program.cs
@@ -0,0 +1,28 @@
+﻿// Программа, принимающая на вход пятизначное число с проверкой того, является ли это числ
+
+bool NumberInInterval(int a, int b, int c)
+{
+    if (a >= b && a <= c)
+        return true;
+    else 
+    {
+        Console.WriteLine($"Ошибка! Число должно находиться в интервале от {b} до {c}!");
+        return false;
+    }   
+}
+
+Console.Clear();
+string m;
+int n;
+do
+{
+    Console.Write("Введите пятизначное число: ");
+    m = Console.ReadLine();
+    n = Convert.ToInt32(m);
+} while (!NumberInInterval(n, 10000, 99999));
+
+if ((n / 10000 == n % 10) && (n % 10000 / 1000 == n % 100 / 10))
+    Console.WriteLine($"Число {n} - палиндром.");
+else
+    Console.WriteLine($"Число {n} - НЕ палиндром.");
+
