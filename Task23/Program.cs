95dd0c67ed79cfd0544f1359c85b3d8c22cd169c
Sofya18 <119758211+Sofya18@users.noreply.github.com>
2022-12-10 23:50:08 +0300

    Task 23

---
+++ Task23/Program.cs
@@ -0,0 +1,15 @@
+﻿// Программа, принимающая на вход число N и выводит таблицу кубов чисел от 1 до N
+
+Console.Clear();
+Console.Write("Введите число N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+for (int i = 1; i <= n; i++)
+    Console.WriteLine($"{i} - {i * i * i}");
+
+
+
+
+
+
+
