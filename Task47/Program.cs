d412afad89a6298eef797a89388a2154c3f46674
Sofya18 <119758211+Sofya18@users.noreply.github.com>
2022-12-23 16:50:43 +0300

    Task47

---
+++ Task47/Program.cs
@@ -0,0 +1,51 @@
+﻿//  Задайте двумерный массив размером m×n, заполненный случайными вещественными числам
+
+ double FloatRnd(double min, double max, int digits)
+{
+    return Math.Round(new Random().NextDouble() * (max - min) + min, digits);
+}
+
+void InputMatrix(double[,] matrix)
+{
+    for (int row = 0; row < matrix.GetLength(0); row++)
+    {
+        for (int col = 0; col < matrix.GetLength(1); col++)
+        {
+            matrix[row, col] = FloatRnd(-100.0, 100.0, 3);
+        }
+        Console.WriteLine();
+    }
+}
+
+void OutputMatrix(double[,] matrix)
+{
+    for (int row = 0; row < matrix.GetLength(0); row++)
+    {
+        for (int col = 0; col < matrix.GetLength(1); col++)
+        {
+            Console.Write($"{matrix[row, col]}   \t");
+        }
+        Console.WriteLine();
+    }
+}
+
+Console.Clear();
+Console.Write("Введите размеры матрицы: ");
+int[] size = Console.ReadLine().Trim().Split(" ").Select(x => int.Parse(x)).ToArray();
+double[,] matrix = new double[size[0], size[1]];
+InputMatrix(matrix);
+OutputMatrix(matrix);
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+
+
+
+
+
+
+
