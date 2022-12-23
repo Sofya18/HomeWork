010598a198772157d3087dcfd06bde7cf089289c
Sofya18 <119758211+Sofya18@users.noreply.github.com>
2022-12-23 16:49:55 +0300

    Task41

---
+++ Task41/Program.cs
@@ -0,0 +1,15 @@
+﻿//  Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+Console.Clear();
+Console.Write("Введите элементы массива: ");
+
+double[] array = Console.ReadLine().Split(" ").Select(x => Convert.ToDouble(x)).ToArray();
+Console.WriteLine($"Массив: {string.Join(" ", array)}");
+int count = 0;
+
+foreach (double element in array)
+{
+    if (element > 0.0) count++;
+}
+
+Console.WriteLine($"Количество чисел больше 0 в массиве = {count}");
