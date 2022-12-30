8f6a98fec2e64c8600fd0ed72f9139cf72e88edb
Sofya18 <119758211+Sofya18@users.noreply.github.com>
2022-12-30 22:48:04 +0300

    Task58

---
+++ Task58/Program.cs
@@ -0,0 +1,58 @@
+﻿//  Задайте две матрицы. Напишите программу, которая будет находить произведение двух матриц.
+void InputMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+            matrix[i, j] = new Random().Next(-10, 11); // [-10, 10]
+    }
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+            Console.Write($"{matrix[i, j]} \t");
+        Console.WriteLine();
+    }
+}
+
+int[,] MultiMatrix(int[,] matrA, int[,] matrB)
+{
+    int[,] result = new int[matrA.GetLength(0), matrB.GetLength(1)];
+    for (int i = 0; i < result.GetLength(0); i++)
+    {
+        for (int j = 0; j < result.GetLength(1); j++)
+        {
+            int tmp = 0;
+            for (int k = 0; k < matrA.GetLength(1); k++)
+            {
+                tmp += matrA[i,k] * matrB[k,j];
+            }
+            result[i,j] = tmp;
+        }
+    }
+    return result;
+}
+Console.Clear();
+Console.Write("Введите размер 1-ой матрицы: ");
+int[] sizeA = Console.ReadLine().Split(" ").Select(x => int.Parse(x)).ToArray();
+int[,] matrixA = new int[sizeA[0], sizeA[1]];
+Console.Write("Введите размер 2-ой матрицы: ");
+int[] sizeB = Console.ReadLine().Split(" ").Select(x => int.Parse(x)).ToArray();
+int[,] matrixB = new int[sizeB[0], sizeB[1]];
+if (sizeA[1] != sizeB[0])
+{
+    Console.WriteLine("ОШИБКА! Количество столбцов первой матрицы не совпадает с количеством строк во второй!");
+    return;
+}
+InputMatrix(matrixA);
+InputMatrix(matrixB);
+Console.WriteLine("1-я исходная матрица:");
+PrintMatrix(matrixA);
+Console.WriteLine("2-я исходная матрица:");
+PrintMatrix(matrixB);
+int[,] matrixC = MultiMatrix(matrixA, matrixB);
+Console.WriteLine("Результат умножения:");
+PrintMatrix(matrixC);
