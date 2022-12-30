efd933a774d61992d36e324642c25fa3b03a9c3f
Sofya18 <119758211+Sofya18@users.noreply.github.com>
2022-12-30 22:48:52 +0300

    Task60

---
+++ Task60/Program.cs
@@ -0,0 +1,40 @@
+﻿//Сформируйте трёхмерный массив из неповторяющихся двузначных чисел. Напишите программу, которая будет построчно выводить массив, добавляя индексы каждого элемента.
+void InputArray(int[,,] arr)
+{
+    int value = 10;
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            for (int k = 0; k < arr.GetLength(2); k++)
+            {
+                arr[i, j, k] = value;
+                value++;
+            }
+
+        }
+    }
+}
+
+void PrintArray(int[,,] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            for (int k = 0; k < arr.GetLength(2); k++)
+            {
+                Console.Write($"{arr[i, j, k]} ({i}, {j}, {k}) \t");
+            }
+            Console.WriteLine();
+        }
+    }
+}
+
+Console.Clear();
+Console.Write("Введите размер массива: ");
+int[] size = Console.ReadLine().Split(" ").Select(x => int.Parse(x)).ToArray();
+int[,,] array = new int[size[0], size[1], size[2]];
+InputArray(array);
+Console.WriteLine("Трехмерный массив:");
+PrintArray(array);
