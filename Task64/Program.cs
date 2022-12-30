4ff9e7570bb746f0b9d1ba598e083d1681b5365c
Sofya18 <119758211+Sofya18@users.noreply.github.com>
2022-12-30 22:49:36 +0300

    Task64

---
+++ Task64/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задайте значение M и N. Напишите программу, которая выведет все натуральные числа в промежутке от N до M. Выполнить с помощью рекурсии.
+string GetNumbers(int start, int end)
+{
+    if (start == end) return end.ToString();
+    return (start < end) ? GetNumbers(start + 1, end) + $", {start}": 
+                           GetNumbers(start, end + 1) + $", {end}";
+}
+
+Console.Clear();
+Console.Write("Введите число M: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine($"Числа: {GetNumbers(m, n)}");
