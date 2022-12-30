574269f43a4e38b89d976cf0c817d16ec5e72778
Sofya18 <119758211+Sofya18@users.noreply.github.com>
2022-12-30 22:49:18 +0300

    Task62

---
+++ Task62/Program.cs
@@ -0,0 +1,44 @@
+﻿// Напишите программу, которая заполнит спирально массив 4 на 4.
+void InputMatrix(int[,] matrix)
+{
+    void FillNext(int row, int col, int value)
+    {
+        matrix[row, col] = value;
+        if (col < matrix.GetLength(1) - 1 && matrix[row, col + 1] == 0)
+            FillNext(row, col + 1, value + 1);
+        if (row < matrix.GetLength(0) - 1 && matrix[row + 1, col] == 0)
+            FillNext(row + 1, col, value + 1);
+        if (col > 0 && matrix[row, col - 1] == 0)
+            FillNext(row, col - 1, value + 1);
+        if (row > 0 && matrix[row - 1, col] == 0)
+            FillUp(row - 1, col, value + 1);
+    }
+
+    void FillUp(int row, int col, int value)
+    {
+        matrix[row, col] = value;
+        if (row > 0 && matrix[row - 1, col] == 0)
+            FillUp(row - 1, col, value + 1);
+        if (col < matrix.GetLength(1) - 1 && matrix[row, col + 1] == 0)
+            FillNext(row, col + 1, value + 1);
+    }
+    FillNext(0, 0, 1);
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+            Console.Write($"{matrix[i, j]} \t");
+        Console.WriteLine();
+    }
+}
+
+Console.Clear();
+Console.Write("Введите размер матрицы: ");
+int[] size = Console.ReadLine().Split(" ").Select(x => int.Parse(x)).ToArray();
+int[,] matrix = new int[size[0], size[1]];
+InputMatrix(matrix);
+Console.WriteLine("Матрица:");
+PrintMatrix(matrix);
