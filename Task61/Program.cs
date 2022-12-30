df10543a9a8716c7c94da40333f56a99f80987a7
Sofya18 <119758211+Sofya18@users.noreply.github.com>
2022-12-30 22:49:06 +0300

    Task61

---
+++ Task61/Program.cs
@@ -0,0 +1,37 @@
+﻿// Вывести первые N строк треугольника Паскаля. Сделать вывод в виде равнобедренного треугольника.
+int NormIndex(int value, int n) => (value >= 0) ? (value % n) : (value % n) + n;
+
+void FillMatrix(int[,] matrix)
+{
+    int colCount = matrix.GetLength(1);
+    matrix[0, colCount / 2] = 1;
+    for (int i = 1; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+            matrix[i, j] = matrix[i-1, NormIndex(j - 1, colCount)] + 
+                           matrix[i-1, NormIndex(j + 1, colCount)];
+    }
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (matrix[i, j] == 0)
+                Console.Write($" \t");
+            else    
+                Console.Write($"{matrix[i, j]} \t");
+        }
+        Console.WriteLine();
+    }
+}
+
+Console.Clear();
+Console.Write("Введите количество строк треугольника Паскаля: ");
+int size = Convert.ToInt32(Console.ReadLine());
+int[,] matrix = new int[size, (size - 1) * 2 +1];
+FillMatrix(matrix);
+Console.WriteLine("Треугольник Паскаля:");
+PrintMatrix(matrix);
